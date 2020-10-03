fbd3802659725857a845af952dd73ac66ff70356
Thomas Albans <albans.thomas@gmail.com>
2020-10-03 21:26:25 +0100

    Adding ASP.NET Core Web API project for communication with Starling API

---
+++ StarlingAPI/Controllers/StarlingAccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StarlingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class StarlingAccountController : ControllerBase
+    {
+
+        private readonly ILogger<StarlingAccountController> _logger;
+
+        public StarlingAccountController(ILogger<StarlingAccountController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
